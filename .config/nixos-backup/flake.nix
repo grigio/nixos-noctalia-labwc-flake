b82a7dfbdb981d093dc6774687f255ac9ede6fe7
grigio <grigio@nixos>
2026-07-30 12:21:41 +0200

    add devShell (deadnix, statix) and CI lint step

--- .config/nixos-backup/flake.nix
+++ .config/nixos-backup/flake.nix
@@ -5,11 +5,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, ... }: let
+    pkgsFor = nixpkgs.legacyPackages.x86_64-linux;
+  in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+      ];
+    };
+
+    devShells.x86_64-linux.default = pkgsFor.mkShell {
+      packages = with pkgsFor; [
+        deadnix
+        statix
       ];
     };
   };
